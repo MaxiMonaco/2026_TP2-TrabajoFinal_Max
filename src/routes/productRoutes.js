fbd17b22f37e8b51a358c03e9b714024f556c3f9
Daiana Gimenez <daiana.gimenez.t@gmail.com>
2026-06-13 16:53:30 -0300

    Role Admin

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,13 +1,12 @@
 import express from "express";
 import * as controller from "../controllers/productController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
+import { adminMiddleware } from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
 
-router.get("/",authMiddleware, adminMiddleware,controller.getAll);
+router.get("/",controller.getAll);
 router.get("/:id", controller.getById);
-
-
-
 
 router.post("/", authMiddleware, adminMiddleware, controller.create);
 router.put("/:id", authMiddleware, adminMiddleware, controller.update);
